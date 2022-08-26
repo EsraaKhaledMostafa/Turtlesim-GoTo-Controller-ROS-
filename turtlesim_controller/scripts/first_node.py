37dc6bb1097ae920af111cc4a1b4e5733d5d866d
Esraa Khaled <87395019+EsraaKhaledMostafa@users.noreply.github.com>
2022-08-26 22:23:58 +0200

    Add files via upload

---
+++ turtlesim_controller/scripts/first_node.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import rospy
+
+if __name__ == '__main__':
+
+    rospy.init_node('Test_node')
+
+    rospy.loginfo("Test node has been started")
+    rate =rospy.Rate(10)
+
+    while not rospy.is_shutdown():
+        rospy.loginfo("Hello")
+        rate.sleep()
+
+    
